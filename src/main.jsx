fddc1e3cc00828a68991f74891f3d09bde82838f
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-24 15:16:08 +0530

    add react-bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
+import Todo from './components/Todo/Todo'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+  <Todo/>
   </StrictMode>,
 )
